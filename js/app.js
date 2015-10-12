7f3a4325eed71485eebe6e17b0122a1cbb34ca4f
Nick Owen <nowen@scottlogic.com>
2015-10-12 16:38:59 +0100

    Rotate chart, fix data

--- js/app.js
+++ js/app.js
@@ -1,33 +1,35 @@
 ﻿(function () {
   'use strict';
 
-  var width = 1000,
-    barHeight = 20;
+  var width = 1500,
+    height = 500;
 
-  var x = d3.scale.linear()
-      .range([0, width]);
+  var y = d3.scale.linear()
+      .range([height, 0]);
 
   var chart = d3.select(".chart")
-    .attr("width", width);
+    .attr("width", width)
+    .attr("height", height);
 
   d3.json("imdb_top_50.json", function (error, data) {
-    x.domain([0, d3.max(data, function (d) { return d.rating; })]);
+    y.domain([0, d3.max(data, function (d) { return d.rating; })]);
 
-    chart.attr("height", barHeight * data.length);
+    var barWidth = width / data.length;
 
     var bar = chart.selectAll("g")
         .data(data)
       .enter().append("g")
-        .attr("transform", function (d, i) { return "translate(0," + i * barHeight + ")"; });
+        .attr("transform", function (d, i) { return "translate(" + i * barWidth + ",0)"; });
 
     bar.append("rect")
-      .attr("width", function (d) { return x(d.rating); })
-      .attr("height", barHeight - 1);
+      .attr("y", function (d) { return y(d.rating); })
+      .attr("height", function (d) { return height - y(d.rating); })
+      .attr("width", barWidth - 1);
 
     bar.append("text")
-      .attr("x", function (d) { return x(d.rating) - 3; })
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
+      .attr("x", barWidth / 2)
+      .attr("y", function (d) { return y(d.rating) + 3; })
+      .attr("dy", ".75em")
       .text(function (d) { return d.rating; });
   });
 }());
