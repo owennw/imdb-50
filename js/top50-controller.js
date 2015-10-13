49381566d2e887587abe24d64b9d8f5af19d52b9
Nick Owen <nowen@scottlogic.com>
2015-10-13 13:43:39 +0100

    Bind radio buttons to model

--- js/top50-controller.js
+++ js/top50-controller.js
@@ -5,6 +5,16 @@
     .controller('Top50Ctrl', ['top50DataService', function (top50DataService) {
       var self = this;
 
+      self.sortings = [
+        {
+          value: 'Rank',
+          checked: true
+        },
+        {
+          value: 'Release Year',
+          checked: false
+        }];
+
       self.data = [];
       top50DataService.getData()
         .then(function (data) {
