dea7685cce9849973a61bffeba548081c993e951
Nick Owen <nowen@scottlogic.com>
2015-10-13 10:19:19 +0100

    Create controller and data service

---
+++ js/top50-controller.js
@@ -0,0 +1,13 @@
+﻿(function () {
+  'use strict';
+
+  angular.module('imdb.top50', [])
+    .controller('Top50Ctrl', ['top50DataService', function (top50DataService) {
+      var self = this;
+
+      top50DataService.getData()
+        .then(function (data) {
+          self.data = data;
+        });
+    }]);
+}());
---
+++ js/top50Data-service.js
@@ -0,0 +1,18 @@
+﻿(function () {
+  'use strict';
+
+  angular.module('imdb.service', [])
+    .factory('top50DataService', ['$http', function ($http) {
+
+      function getData() {
+        return $http.get('imdb_top_50.json')
+          .then(function (top50) {
+            return top50.data;
+          });
+      }
+
+      return {
+        getData: getData
+      };
+    }]);
+}());
